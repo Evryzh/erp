31757a2161e4c47b0cc04429246944bae08fe03e
Ryzhkov_EV@inbox.ru <User@10.126.96.118>
2022-12-12 11:17:07 +0300

    daly

--- src/main/java/com/github/evryzh/erp/WebServiceConfig.java
+++ src/main/java/com/github/evryzh/erp/WebServiceConfig.java
@@ -1,7 +1,42 @@
 package com.github.evryzh.erp;
 
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.XsdSchema;
+import org.springframework.xml.xsd.SimpleXsdSchema;
 
+@EnableWs
+@Configuration
 public class WebServiceConfig extends WsConfigurerAdapter {
+	@Bean
+	public ServletRegistrationBean<MessageDispatcherServlet> messageDispatcherServlet(ApplicationContext applicationContext) {
+		MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+		servlet.setApplicationContext(applicationContext);
+		servlet.setTransformWsdlLocations(true);
+		return new ServletRegistrationBean<>(servlet, "/ws/*");
+	}
+	
+	@Bean(name = "exchange2is")
+	public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema exchange2isSchema) {
+		DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
+		wsdl11Definition.setPortTypeName("Exchange2isPort");
+		wsdl11Definition.setLocationUri("/ws");
+		wsdl11Definition.setTargetNamespace("http://localhost:8080/exchange2is");
+		wsdl11Definition.setSchema(exchange2isSchema);
+		return wsdl11Definition;
+	}
 
+	@Bean
+	public XsdSchema countriesSchema() {
+		return new SimpleXsdSchema(new ClassPathResource("countries.xsd"));
+	}
 }
+
+	
